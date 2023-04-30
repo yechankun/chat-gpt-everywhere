ca921e3e9ed2578ecd695b47bd3d01bd76bf6fec
yechankun <redniche@naver.com>
2023-05-01 02:03:30 +0900

    feat: Implement popup button control (resolve #4)

--- src/content-script/App.svelte
+++ src/content-script/App.svelte
@@ -2,13 +2,45 @@
   import FloatingButton from './components/FloatingButton.svelte';
 
   let section: HTMLElement;
+  let isDragging = false;
+  let isClick = true;
+  let offsetX: number;
+  let offsetY: number;
+
+  function handlePointerDown(event: PointerEvent) {
+    isDragging = true;
+    isClick = true;
+    offsetX = event.clientX - section.getBoundingClientRect().left;
+    offsetY = event.clientY - section.getBoundingClientRect().top;
+  }
+
+  function handlePointerMove(event: PointerEvent) {
+    if (!isDragging) return;
+    console.debug('dragging');
+    isClick = false;
+    section.style.left = event.clientX - offsetX + 'px';
+    section.style.top = event.clientY - offsetY + 'px';
+  }
+
+  function handlePointerUp() {
+    isDragging = false;
+  }
 </script>
+
+<svelte:window
+  on:pointermove={handlePointerMove}
+  on:pointerup={handlePointerUp}
+/>
 
 <div id="everywhere">
   <section
     class="fixed top-1/2 left-1/2 z-[9999999999] w-auto"
     bind:this={section}
+    on:pointerdown={handlePointerDown}
   >
-    <FloatingButton />
+    <FloatingButton {isClick} />
   </section>
 </div>
+
+<style>
+</style>
--- src/content-script/components/FloatingButton.svelte
+++ src/content-script/components/FloatingButton.svelte
@@ -3,11 +3,29 @@
   import { fade, fly } from 'svelte/transition';
 
   let button: HTMLElement;
+  export let isClick: boolean;
+  let showPopup = false;
+
+  function handleClick() {
+    if (isClick) {
+      console.debug('버튼 클릭!');
+      showPopup = !showPopup;
+    }
+  }
 </script>
 
 <button
   bind:this={button}
   class="cursor-pointer select-none bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded"
+  on:click={handleClick}
 >
   <img src={base64} alt="Logo" class="pointer-events-none select-none" />
 </button>
+{#if showPopup}
+  <h1 in:fly={{ y: 30, duration: 2000 }} out:fade>
+    <mark>Sex</mark> on the <mark>Beach</mark>
+  </h1>
+{/if}
+
+<style>
+</style>
