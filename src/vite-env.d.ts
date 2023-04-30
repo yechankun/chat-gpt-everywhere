aaadb5c807c69b9da492ac9fdab887c8b25ee5b9
Yechankun <redniche@naver.com>
2023-05-01 01:50:31 +0900

    feat: Implement popup button UI (resolve #2) (#8)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,27 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+
+declare module '*.svelte' {
+  import type { ComponentType } from 'svelte';
+  const component: ComponentType;
+  export default component;
+}
+declare module '*&imagetools' {
+  /**
+   * actual types
+   * - code https://github.com/JonasKruckenberg/imagetools/blob/main/packages/core/src/output-formats.ts
+   * - docs https://github.com/JonasKruckenberg/imagetools/blob/main/docs/guide/getting-started.md#metadata
+   */
+  const out;
+  export default out;
+}
+
+declare module '*&run' {
+  const src: string;
+  const width: number;
+  const height: number;
+  const format: string;
+  const base64: string;
+
+  export { src, width, height, format, base64 };
+}
